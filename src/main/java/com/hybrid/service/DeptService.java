1c945c2b54d89e16eb0c7f8028b9c2a4a817f566
java <java@shyang>
2016-02-29 17:28:32 +0900

    RDS 적용

---
+++ src/main/java/com/hybrid/service/DeptService.java
@@ -0,0 +1,49 @@
+package com.hybrid.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.hybrid.mapper.DeptMapper;
+import com.hybrid.model.Dept;
+
+@Service
+public class DeptService {
+	
+	//필드주입
+	@Autowired
+	DeptMapper deptMapper;
+	
+	@Transactional
+	public List<Dept> getList() {
+		
+		List<Dept> depts = deptMapper.selectAll();
+		
+		return depts;
+	}
+	
+	@Transactional
+	public Dept getDept(Integer deptno) {
+		return deptMapper.selectByDeptno(deptno);
+	}
+	
+	@Transactional
+	public void insert(Dept dept) {
+		deptMapper.insert(dept);
+	}
+	
+	@Transactional
+	public Dept delete(Integer deptno) {
+		
+		Dept dept = deptMapper.selectByDeptno(deptno);
+		deptMapper.deleteByDeptno(deptno);
+		return dept;
+	}
+	
+	
+	
+	
+	
+}
